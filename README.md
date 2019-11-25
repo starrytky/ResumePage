6a5eefadf45620646ac9ae290252c9ca1cb9e754
starrytky <starrytky@qq.com>
2019-11-26 02:24:55 +0800

    导航栏横条，定位，submenu

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ResumePage
+
+## 导航栏下的div
+```css
+.topNavBar nav > ul > li > a {    font-size: 12px;    color: inherit;    font-weight: bold;    border-bottom: 3px solid transparent;    border-top: 3px solid transparent;    padding-top: 5px;    padding-bottom: 5px;    display: block;
+    position: relative;}
+.topNavBar nav > ul > li > a:hover::after {    content: '';    display: block;    position: absolute;    left: 0;    top: 100%;   width: 100%;   background: rgb(232, 103, 107);    height: 3px;}
+```
--- js/index.js
+++ js/index.js
@@ -17,4 +17,25 @@ setTimeout(function () {
 
 window.onscroll = function(){
     window.scrollY > 0 ? my$("topNavBar").classList.add("sticky"):my$("topNavBar").classList.remove("sticky")
+};
+
+let liTags = document.querySelectorAll("nav.menu > ul > li");
+for (let i=0;i < liTags.length; i++){
+    liTags[i].onmouseenter = function(x){
+        x.currentTarget.classList.add("active");
+    };
+    liTags[i].onmouseleave = function(x){
+        x.currentTarget.classList.remove("active");
+    }
+}
+let aTags = document.querySelectorAll("nav.menu > ul > li > a");
+for (let i = 0;i<aTags.length;i++){
+    aTags[i].onclick = function (x) {
+        x.preventDefault();
+        let a = x.currentTarget;
+        let href = a.getAttribute("href");
+        let element = document.querySelector(href);
+        let top = element.offsetTop;
+        window.scrollTo(0, top - 80);
+    }
 }
