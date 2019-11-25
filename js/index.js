fad69c30de009c55506b442fda2ae529ce4f346c
starrytky <starrytky@qq.com>
2019-11-25 17:25:41 +0800

    加载动画、sticky topNavBar

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
-var my$ = function(id){
+var my$ = function (id) {
     return document.getElementById(id);
 };
-my$("portfolioAll").onclick =function(){
+my$("portfolioAll").onclick = function () {
     my$("portfolioBar").className = "bar state-1"
 };
-my$("portfolioFramework").onclick =function(){
+my$("portfolioFramework").onclick = function () {
     my$("portfolioBar").className = "bar state-2"
 };
-my$("portfolioValina").onclick =function(){
+my$("portfolioValina").onclick = function () {
     my$("portfolioBar").className = "bar state-3"
 };
+
+setTimeout(function () {
+    my$("siteWelcome").classList.remove('active');
+}, 1000);
+
+window.onscroll = function(){
+    window.scrollY > 0 ? my$("topNavBar").classList.add("sticky"):my$("topNavBar").classList.remove("sticky")
+}
