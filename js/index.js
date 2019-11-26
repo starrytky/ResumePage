b8719f8b8f3e8832614e73dea1f530947f85913e
starrytky <starrytky@qq.com>
2019-11-26 14:28:29 +0800

    导入tween.js使用缓动

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var my$ = function (id) {
+let my$ = function (id) {
     return document.getElementById(id);
 };
 my$("portfolioAll").onclick = function () {
@@ -29,13 +29,37 @@ for (let i=0;i < liTags.length; i++){
     }
 }
 let aTags = document.querySelectorAll("nav.menu > ul > li > a");
+function animate(time) {
+    requestAnimationFrame(animate);
+    TWEEN.update(time);
+}
+requestAnimationFrame(animate);
+
 for (let i = 0;i<aTags.length;i++){
     aTags[i].onclick = function (x) {
         x.preventDefault();
-        let a = x.currentTarget;
-        let href = a.getAttribute("href");
-        let element = document.querySelector(href);
-        let top = element.offsetTop;
-        window.scrollTo(0, top - 80);
+        // let a = x.currentTarget;
+        // let href = a.getAttribute("href");
+        // let element = document.querySelector(href);
+        // let top = element.offsetTop;
+        let top  = document.querySelector(x.currentTarget.getAttribute("href")).offsetTop;
+        let currentTop = window.scrollY;
+        let targetTop = top - 80;
+        let s = targetTop - currentTop;
+        let coords = { y: currentTop};
+        let t = Math.abs((s/100)*150);
+        if (t>1500){
+            t = 1100;
+        }
+        console.log(t);
+        let tween = new TWEEN.Tween(coords)
+            .to({ y: targetTop}, t)
+            .easing(TWEEN.Easing.Quadratic.InOut)
+            .onUpdate(function() {
+                window.scrollTo(0,coords.y);
+            })
+            .start();
+
     }
 }
+
