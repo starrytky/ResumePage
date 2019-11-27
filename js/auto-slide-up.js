e293b27e3b9fe8a787399413c58ebca0df2029d0
starrytky <starrytky@qq.com>
2019-11-27 19:17:01 +0800

    拆解代码，mvc

--- js/auto-slide-up.js
+++ js/auto-slide-up.js
@@ -1,55 +1,65 @@
-let specialTags = document.querySelectorAll("[data-x]");
-addOffset();
-addAndRemoveActive();
-
-window.addEventListener("scroll", function(){
-    removeOffset();
-    findCloset();
-});
-
-//添加offset
-function addOffset(){
-    for (let i = 0;i<specialTags.length; i++){
-        specialTags[i].classList.add("offset");
-    }
-}
-//删除offset
-function removeOffset(){
-    let minIndex = 0;
-    for (let i = 1;i<specialTags.length; i++){
-        if (Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[i-1].offsetTop - window.scrollY)){
-            minIndex = i;
-        }
-    }
-    specialTags[minIndex].classList.remove("offset");
-}
-//增加移除active
-function addAndRemoveActive(){
-    let liTags = document.querySelectorAll("nav.menu > ul > li");
-    for (let i=0;i < liTags.length; i++){
-        liTags[i].onmouseenter = function(x){
-            x.currentTarget.classList.add("active");
-        };
-        liTags[i].onmouseleave = function(x){
-            x.currentTarget.classList.remove("active");
-        }
-    }
-}
-//增加移除高亮
-function findCloset(){
+!function(){
     let specialTags = document.querySelectorAll("[data-x]");
-    let minIndex = 0;
-    for (let i = 1;i<specialTags.length; i++){
-        if (Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[i-1].offsetTop - window.scrollY)){
-            minIndex = i;
+    let controller = {
+        init: function () {
+            this.addAndRemoveActive();
+            this.addOffset();
+            this.bindEvents();
+        },
+        bindEvents: function () {
+            window.addEventListener("scroll", () => {
+                this.removeOffset();
+                this.findCloset();
+            });
+        },
+        //增加移除active
+        addAndRemoveActive: function () {
+            let liTags = document.querySelectorAll("nav.menu > ul > li");
+            for (let i = 0; i < liTags.length; i++) {
+                liTags[i].onmouseenter = function (x) {
+                    x.currentTarget.classList.add("active");
+                };
+                liTags[i].onmouseleave = function (x) {
+                    x.currentTarget.classList.remove("active");
+                }
+            }
+        },
+        //添加offset
+        addOffset: function () {
+            for (let i = 0; i < specialTags.length; i++) {
+                specialTags[i].classList.add("offset");
+            }
+        },
+        //删除offset
+        removeOffset: function () {
+            let minIndex = 0;
+            for (let i = 1; i < specialTags.length; i++) {
+                if (Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[i - 1].offsetTop - window.scrollY)) {
+                    minIndex = i;
+                }
+            }
+            specialTags[minIndex].classList.remove("offset");
+        },
+        //增加移除高亮
+        findCloset: function () {
+            let specialTags = document.querySelectorAll("[data-x]");
+            let minIndex = 0;
+            for (let i = 1; i < specialTags.length; i++) {
+                if (Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[i - 1].offsetTop - window.scrollY)) {
+                    minIndex = i;
+                }
+            }
+            let id = specialTags[minIndex].id;
+            let a = document.querySelector('a[href=\"#' + id + '\"]');
+            let li = a.parentNode;
+            let brothersAndMe = li.parentNode.children;
+            for (let i = 0; i < brothersAndMe.length; i++) {
+                brothersAndMe[i].classList.remove("highlight");
+            }
+            li.classList.add("highlight");
         }
-    }
-    let id = specialTags[minIndex].id;
-    let a = document.querySelector('a[href=\"#'+id+'\"]');
-    let li = a.parentNode;
-    let brothersAndMe = li.parentNode.children;
-    for (let i=0;i<brothersAndMe.length;i++){
-        brothersAndMe[i].classList.remove("highlight");
-    }
-    li.classList.add("highlight");
-}
+    };
+    controller.init();
+}.call();
+
+
